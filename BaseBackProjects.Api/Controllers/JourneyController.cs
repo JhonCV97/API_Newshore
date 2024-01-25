f71967dc791cfd28e6aa592091d9c8725d6a624d
JhonCV97 <jhon.castano16445@ucaldas.edu.co>
2024-01-24 20:55:03 -0500

    Add controller Journey

---
+++ BaseBackProjects.Api/Controllers/JourneyController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Cqrs.Journey.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaseBackProjects.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JourneyController : ApiControllerBase
+    {
+        /// <summary>
+        /// Se realiza consulta de ruta
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetRoute([FromQuery] GetRouteQuery query)
+        {
+            return Ok(await Mediator.Send(query));
+        }
+    }
+}
